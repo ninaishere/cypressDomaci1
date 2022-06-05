bfea54709e55dd662e8b0162339f9a56c438409e
ninaishere <raickovic2001@gmail.com>
2022-06-05 18:06:46 +0200

    negative cases

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -9,13 +9,74 @@ describe("Register test", () => {
     cy.get('a[href="/register"]').click();
   });
 
-  it("Register with valid info", () => {
+  it.only("Register with valid info", () => {
+    cy.visit("/");
+    cy.get('a[href="/register"]').click();
     cy.get("#first-name").type("Nina");
     cy.get("#last-name").type("R");
     cy.get("#email").type("nina@gmail.com"); // staviti drugi mail da bi radilo
     cy.get("#password").type("test123456");
     cy.get("#password-confirmation").type("test123456");
     cy.get(".form-check-input").click();
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Register without first name", () => {
+    cy.visit("/");
+    cy.get('a[href="/register"]').click();
+    cy.get("#first-name").type("");
+    cy.get("#last-name").type("R");
+    cy.get("#email").type("nina@gmail.com");
+    cy.get("#password").type("test123456");
+    cy.get("#password-confirmation").type("test123456");
+    cy.get(".form-check-input").click();
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Register without last name", () => {
+    cy.visit("/");
+    cy.get('a[href="/register"]').click();
+    cy.get("#first-name").type("Nina");
+    cy.get("#last-name").type("");
+    cy.get("#email").type("nina@gmail.com");
+    cy.get("#password").type("test123456");
+    cy.get("#password-confirmation").type("test123456");
+    cy.get(".form-check-input").click();
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Register with wrong email", () => {
+    cy.visit("/");
+    cy.get('a[href="/register"]').click();
+    cy.get("#first-name").type("Nina");
+    cy.get("#last-name").type("R");
+    cy.get("#email").type("asdf");
+    cy.get("#password").type("test123456");
+    cy.get("#password-confirmation").type("test123456");
+    cy.get(".form-check-input").click();
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Register with invalid password", () => {
+    cy.visit("/");
+    cy.get('a[href="/register"]').click();
+    cy.get("#first-name").type("Nina");
+    cy.get("#last-name").type("R");
+    cy.get("#email").type("test1@gmail.com");
+    cy.get("#password").type("test");
+    cy.get("#password-confirmation").type("test");
+    cy.get(".form-check-input").click();
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Register witout accepting terms and conditions", () => {
+    cy.visit("/");
+    cy.get('a[href="/register"]').click();
+    cy.get("#first-name").type("Nina");
+    cy.get("#last-name").type("R");
+    cy.get("#email").type("test1@gmail.com");
+    cy.get("#password").type("test123123");
+    cy.get("#password-confirmation").type("test123123");
     cy.get('button[type="submit"]').click();
   });
 });
